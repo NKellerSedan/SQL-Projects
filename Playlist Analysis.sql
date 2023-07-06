85b24d9668f78dce328e344dc48705e33f74d62c
Nicholas Keller-Sedan <104579750+NKellerSedan@users.noreply.github.com>
2023-07-06 13:13:01 -0400

    data validation (50 tracks added each day)

--- Playlist Analysis.sql
+++ Playlist Analysis.sql
@@ -1,1 +1,6 @@
 SELECT * FROM dbo.playlist;
+
+-- 1. How many tracks were added each day (confirm that each day had 50 added tracks)
+
+SELECT DISTINCT track_add_date, COUNT(track_add_date) AS 'Number of Tracks Added' FROM playlist
+GROUP BY track_add_date
