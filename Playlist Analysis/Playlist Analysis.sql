8d8bbc85f433263c95a70776cb94cc7452bb879f
Nicholas Keller-Sedan <104579750+NKellerSedan@users.noreply.github.com>
2023-07-06 13:19:36 -0400

    longest, shortest, avg track duration

--- Playlist Analysis/Playlist Analysis.sql
+++ Playlist Analysis/Playlist Analysis.sql
@@ -5,6 +5,13 @@ SELECT * FROM dbo.playlist;
 SELECT DISTINCT track_add_date, COUNT(track_add_date) AS 'Num of Tracks Added' FROM playlist
 GROUP BY track_add_date;
 
+
 -- 2. Average, min, max number of tracks on an album
 
 SELECT MIN(number_of_tracks_in_album) AS 'Least Tracks', MAX(number_of_tracks_in_album) AS 'Most Tracks', AVG(number_of_tracks_in_album) AS 'Average Tracks' FROM playlist;
+
+
+-- 3. Longest, Shortest, average track duration
+
+--Milliseconds
+SELECT MIN(track_duration_ms) AS 'Shortest Duration (MS)', MAX(track_duration_ms) AS 'Longest Duration (MS)', AVG(track_duration_ms) AS 'Average Duration (MS)' FROM playlist;
