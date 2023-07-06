b7b7805912b5c9177a0b6acfad542f3bccba5394
Nicholas Keller-Sedan <104579750+NKellerSedan@users.noreply.github.com>
2023-07-06 13:18:24 -0400

    finding avg, min, max tracks on an album

--- Playlist Analysis/Playlist Analysis.sql
+++ Playlist Analysis/Playlist Analysis.sql
@@ -2,5 +2,9 @@ SELECT * FROM dbo.playlist;
 
 -- 1. How many tracks were added each day (confirm that each day had 50 added tracks)
 
-SELECT DISTINCT track_add_date, COUNT(track_add_date) AS 'Number of Tracks Added' FROM playlist
-GROUP BY track_add_date
+SELECT DISTINCT track_add_date, COUNT(track_add_date) AS 'Num of Tracks Added' FROM playlist
+GROUP BY track_add_date;
+
+-- 2. Average, min, max number of tracks on an album
+
+SELECT MIN(number_of_tracks_in_album) AS 'Least Tracks', MAX(number_of_tracks_in_album) AS 'Most Tracks', AVG(number_of_tracks_in_album) AS 'Average Tracks' FROM playlist;
